f3b044c7f0fa421b88f0caa1ace928186d0a722f
Tran Anh Tuat <tuattrananh@gmail.com>
2022-07-30 00:10:41 +0900

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+
     <App />
   </Provider>
 );
